fc02e00363f27b9a692bc7e075ed35146ead00cb
Sondre Torgersen <zonaren@hotmail.com>
2026-05-12 21:10:24 +0200

    endre envdir og legg til chunks i vite config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src',
+  envDir: '..',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/xlsx')) return 'xlsx'
+          if (id.includes('node_modules/chart.js')) return 'charts'
+          if (id.includes('node_modules/@supabase') || id.includes('node_modules/bootstrap')) return 'vendor'
+        },
+      },
+    },
   },
 })
