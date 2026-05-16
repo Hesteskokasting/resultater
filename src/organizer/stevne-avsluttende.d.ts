f852aca75c5ff0d7c7ea4f8b34f93c7b0c873e30
zonaren <sondre.torgersen@gmail.com>
2026-05-16 13:03:46 +0200

    Remove zombie .d.ts files left over from JS migration

--- src/organizer/stevne-avsluttende.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function render(
-  container: HTMLElement,
-  options: { id: number; isAdmin?: boolean },
-  bannerSlot?: HTMLElement | null,
-): Promise<void>
--- src/utils/organizer-test-utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function autoFullforInnledendeKamper(stevneid: number): Promise<void>
-export declare function slettKamperForFase(stevneid: number, fase: string): Promise<void>
-export declare function nullstillStevne(stevneid: number): Promise<void>
