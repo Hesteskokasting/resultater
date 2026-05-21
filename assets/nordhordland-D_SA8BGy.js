59d074f028763bda4aa107704bc63be4f9c2052d
zonaren <43248907+zonaren@users.noreply.github.com>
2026-05-21 15:19:54 +0000

    Deploying to gh-pages from @ Hesteskokasting/resultater@b9b8c44ea0955b63dae1ebcde4b92299cfacc837 🚀

--- assets/nordhordland-D_SA8BGy.js
+++
@@ -1,1 +0,0 @@
-import{K as e}from"./index-Zbekm5gX.js";async function t(t,n,r=null){t.replaceChildren(e(`Nordhordlandsmetoden som avsluttande fase er ikkje implementert enno.`))}export{t as render};
