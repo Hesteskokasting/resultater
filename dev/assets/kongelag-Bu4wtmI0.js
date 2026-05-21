b83d51fc5f2133d0310627a65f3b803b2a5de3a6
zonaren <43248907+zonaren@users.noreply.github.com>
2026-05-21 13:39:33 +0000

    Deploying to gh-pages from @ Hesteskokasting/resultater@4be293d1b16c9ee6c394ad2e2d7745697e891d0a 🚀

---
+++ dev/assets/kongelag-Bu4wtmI0.js
@@ -0,0 +1,1 @@
+import{J as e}from"./index-CufTpKqI.js";async function t(t,n,r=null){t.replaceChildren(e(`Kongelag-organisering er ikkje implementert enno.`))}export{t as render};
--- dev/assets/xkast-ChnOAw0m.js
+++
@@ -1,1 +0,0 @@
-import{J as e}from"./index-CLdjq5VV.js";async function t(t,n,r=null){t.replaceChildren(e(`X-kast-organisering er ikkje implementert enno.`))}export{t as render};
