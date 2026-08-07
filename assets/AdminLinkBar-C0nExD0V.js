d5b7d17fb1118399190ba82f86b5e76af2728816
zonaren <43248907+zonaren@users.noreply.github.com>
2026-08-07 09:42:24 +0000

    Deploying to gh-pages from @ Hesteskokasting/resultater@a292d6f4c0bd8367a088312b4af7fc1f065cad9c 🚀

--- assets/AdminLinkBar-C0nExD0V.js
+++
@@ -1,1 +0,0 @@
-import{P as e,s as t}from"./index-CNUjjVgw.js";function n(n,{href:r,label:i,variant:a,canShow:o}){e().then(e=>{if(!e?.profil||!o(e))return;let s=document.createElement(`div`);s.className=`mb-2 px-2`,s.innerHTML=`<a href="${r}" class="btn btn-sm btn-${a}">${t(i)}</a>`,n.querySelector(`.content-page`)?.prepend(s)})}export{n as t};
